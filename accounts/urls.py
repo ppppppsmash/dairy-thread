c3c9a25f10679345e73f2d249db6a5150648b11d
ppppppsmash <pppppp.work@gmail.com>
2023-10-15 16:17:29 +0900

    feat: add user registration form and validation logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-  path('', views.home, name='home')
+  # views モジュール内の home 関数が呼び出され、このURLに関連付けられる
+  # /dairy_thread → urls.py → accounts/urls.py → accounts/view.py
+  path('', views.home, name='home'),
+  path('regist', views.regist, name='regist')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from . import forms
 
 def home(request):
   return render(
     request, 'accounts/home.html'
   )
+
+def regist(request):
+  regist_form = forms.RegistForm(request.POST)
