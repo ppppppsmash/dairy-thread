8735718c969925565a92c7ce4fc86716653aab64
ppppppsmash <pppppp.work@gmail.com>
2023-10-19 23:06:31 +0900

    feat: user edit form

--- accounts/views.py
+++ accounts/views.py
@@ -60,3 +60,13 @@ def user_logout(request):
   logout(request)
   messages.success(request, 'ログアウトしました')
   return redirect('accounts:home')
+
+@login_required
+def user_edit(request):
+  user_edit_form = forms.UserEditForm(request.POST or None,request.FILES or None, instance=request.user)
+  if user_edit_form.is_valid():
+    messages.success(request, '更新完了しました。')
+    user_edit_form.save()
+  return render(request, 'accounts/user_edit.html', context={
+    'user_edit_form': user_edit_form,
+  })
