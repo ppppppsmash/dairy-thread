98579c29d8096c73fb59ede3208e051f73026068
ppppppsmash <pppppp.work@gmail.com>
2023-10-19 23:25:45 +0900

    feat: password change form

--- accounts/forms.py
+++ accounts/forms.py
@@ -25,7 +25,7 @@ class RegistForm(forms.ModelForm):
     validate_password(self.cleaned_data['password'], user)
     user.set_password(self.cleaned_data['password'])
     user.save()
-    return
+    return user
 
 class LoginForm(forms.Form):
   email = forms.CharField(label='メールアドレス')
@@ -40,3 +40,25 @@ class UserEditForm(forms.ModelForm):
   class Meta:
     model = Users
     fields = ('username', 'age', 'email', 'picture')
+
+class PasswordChangeForm(forms.ModelForm):
+  password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+  confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
+
+  class Meta():
+    model = Users
+    fields = ('password', )
+
+  def clean(self):
+    cleaned_data = super().clean()
+    password = cleaned_data['password']
+    confirm_password = cleaned_data['confirm_password']
+    if password != confirm_password:
+      raise forms.ValidationError('パスワードが異なる')
+    
+  def save(self, commit=False):
+    user = super().save(commit=False)
+    validate_password(self.cleaned_data['password'], user)
+    user.set_password(self.cleaned_data['password'])
+    user.save()
+    return user
